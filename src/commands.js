ffef461a7fe9141d5418c84078f796cb421c6255
DEsimas <simasuh@gmail.com>
2021-12-02 18:55:50 +0300

    help command

--- src/commands.js
+++ src/commands.js
@@ -5,14 +5,17 @@ import rule34 from "./commands/rule.34.js";
 export default [
     {
         name: ["help", "h"],
+        description: "shows this message",
         out: help
     },
     {
         name: ["nhentai", "gethentai", "getdoujin"],
+        description: "fetch doujin from nhentai.net using its code, you can also pass \"random\" to get random doujin",
         out: nhentai
     },
     {
         name: ["rule34", "r34"],
+        description: "fetch pictures from rule34.xxx using tags",
         out: rule34
     }
 ];
--- src/commands/help.js
+++ src/commands/help.js
@@ -1,3 +1,14 @@
+import Discord from "discord.js";
+import commands from "./../commands.js";
+
 export default function help(message, args) {
-    message.channel.send("ok");
+    const embed = new Discord.MessageEmbed()
+        .setColor("#202225")
+        .setAuthor("Commands list:");
+
+    commands.forEach(command => {
+        embed.addField(process.env.PREFIX + command.name[0], command.description);
+    });
+
+    message.channel.send({embeds: [embed]});
 }

8798d0959f28c6bd9d6cc618fe926110d8892430
DEsimas <simasuh@gmail.com>
2021-12-03 01:26:59 +0300

    crash info

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import dotenv from "dotenv";
 
 import messageHandler from "./messageHandler.js";
 
+try {
 dotenv.config();
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on("messageCreate", messageHandler);
 
 client.login(process.env.TOKEN);
+} catch(err) {
+    console.log(err);
+}

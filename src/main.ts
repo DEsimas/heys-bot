99a82ee13b9904140819ecc8325b8466c71941df
DEsimas <simasuh@gmail.com>
2022-01-02 17:57:46 +0300

    database connection added

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,23 @@ import * as Discord from "discord.js";
 import * as dotenv from "dotenv";
 import { CommandsHandler } from "discordjs-commands-parser";
 import { commands } from "./commands";
+import { DAO } from "./database/DAO";
 
-dotenv.config();
+async function main() {
+    dotenv.config();
 
-const handler = new CommandsHandler({
-    commandsList: commands,
-    prefix: process.env.PREFIX || "$"
-})
+    await DAO.connect();
+    
+    const handler = new CommandsHandler({
+        commandsList: commands,
+        prefix: process.env.PREFIX || "$"
+    })
+    
+    const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
+    client.on("messageCreate", handler.getEventHandler());
+    client.on("ready", () => {console.log("Bot Started!")});
+    client.login(process.env.TOKEN);
 
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
-client.on("messageCreate", handler.getEventHandler());
-client.on("ready", () => {console.log("Bot Started!")});
-client.login(process.env.TOKEN);
+}
+
+main();

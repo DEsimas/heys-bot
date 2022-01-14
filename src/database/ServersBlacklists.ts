babdfd7fe376b41dd87028f14a9443b8b08c4bda
DEsimas <simasuh@gmail.com>
2022-01-14 20:19:04 +0300

    serversblacklis endpoint added

---
+++ src/database/ServersBlacklists.ts
@@ -0,0 +1,66 @@
+import { model, Model, Schema } from "mongoose";
+import { Sites } from "./../sites";
+
+export interface Blacklist {
+    serverID: string;
+    global: string[];
+    sites: Record<Sites, string[]>;
+}
+
+export class ServersBlacklists {
+    private readonly BlacklistModel: Model<Blacklist>
+
+    constructor() {
+        this.BlacklistModel = model<Blacklist>("ServersBlacklists", this.getBlacklistSchema())
+    }
+
+    private getBlacklistSchema(): Schema<Blacklist> {
+        return new Schema<Blacklist>({
+            serverID: String,
+            global: [String],
+            sites: {
+                e621: [String],
+                e926: [String],
+                hypnohub: [String],
+                danbooru: [String],
+                konachanCO: [String],
+                konachanNET: [String],
+                yandere: [String],
+                gelbooru: [String],
+                rule34: [String],
+                safebooru: [String],
+                tbib: [String],
+                xbooru: [String],
+                paheal: [String],
+                derpibooru: [String],
+                realbooru: [String],
+                nhentai: [String]
+            }
+        });
+    }
+
+    private getDefaultBlacklist(serverID: string): Blacklist {
+        return {
+            serverID: serverID,
+            global: ["loli", "lolicon", "shota", "shotacon"],
+            sites: {
+                e621: [],
+                e926: [],
+                hypnohub: [],
+                danbooru: [],
+                konachanCOM: [],
+                konachanNET: [],
+                yandere: [],
+                gelbooru: [],
+                rule34: [],
+                safebooru: [],
+                tbib: [],
+                xbooru: [],
+                paheal: [],
+                derpibooru: [],
+                realbooru: [],
+                nhentai: []
+            }
+        }
+    }
+};

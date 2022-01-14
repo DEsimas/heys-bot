2a9482a153078b7b45e4aa9e4775b47ba684e449
DEsimas <simasuh@gmail.com>
2022-01-14 17:59:01 +0300

    sites array added

---
+++ src/sites.ts
@@ -0,0 +1,10 @@
+export const sitesArray = [
+    'e621',         'e926',
+    'hypnohub',     'danbooru',
+    'konachanCOM',  'konachanNER',
+    'yandere',      'gelbooru',
+    'rule34',       'safebooru',
+    'tbib',         'xbooru',
+    'paheal',       'derpibooru',
+    'realbooru',    'nhentai'
+] as const;

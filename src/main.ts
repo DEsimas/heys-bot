18be45eb7aad4b142919b4c88f04a71ba270d52e
DEsimas <simasuh@gmail.com>
2022-01-18 19:27:05 +0300

    intents comments added

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,13 @@ config(); // import environmental variables
 
 const client = new Client({
     intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+        Intents.FLAGS.GUILDS,                   // TODO: discorver why we need this intent
+        Intents.FLAGS.GUILD_MESSAGES,           // read messages
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS   // read reactions under messages
     ]
 });
 
-client.on('ready', () => console.log('Bot started!'));
-client.on('messageCreate', () => { /*TODO: add commands handler*/ });
+client.on("ready", () => console.log("Bot started!"));
+client.on("messageCreate", () => { /*TODO: add commands handler*/ });
 
 client.login(process.env.TOKEN);

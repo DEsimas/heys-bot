a07d6514ae33a6c02fbbf8eece20897745b95974
DEsimas <simasuh@gmail.com>
2022-01-20 11:37:41 +0300

    doujin sender call added

--- src/commands/Get/Get.ts
+++ src/commands/Get/Get.ts
@@ -2,21 +2,33 @@ import { Payload } from "discordjs-commands-parser";
 import { Blacklist, Blacklists } from "../../database/Blacklists";
 import { DAO } from "../../database/DAO";
 import { command } from "./../Command";
+import { DoujinSender } from "./DoujinSender";
+import { SenderOptions } from "./Sender";
 
 export class Get extends command {
     private readonly site: string;
     private readonly tags: string[];
+    private readonly doujinID: string;
 
     constructor(payload: Payload) {
         super(payload);
         this.site = this.args[1];
+        this.doujinID = this.args[2];
         this.tags = [...this.args];
         this.tags.splice(0,2);
     }
 
     public async execute(): Promise<void> {
         if(this.getSrc(this.site) === "nhentai") {
-            this.message.channel.send("send nhentai dojin");
+            const options: SenderOptions = {
+                message: this.message,
+                blacklist: await this.getBlacklist(),
+                origin: this.site,
+                src: this.getSrc(this.site) || "",
+                tags: undefined,
+                doujinID: this.doujinID
+            }
+            new DoujinSender(options).send();
         } else {
             this.message.channel.send("send booru");
         }

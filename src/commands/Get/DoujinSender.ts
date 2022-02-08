f79cbcbe0cb424a31a75b574d1dd1c54cd0c66ae
DEsimas <simasuh@gmail.com>
2022-02-08 19:10:55 +0300

    fetchrandomdoujin: using randomDoujinID instead of random number generator

--- src/commands/Get/DoujinSender.ts
+++ src/commands/Get/DoujinSender.ts
@@ -28,7 +28,7 @@ export class DoujinSender extends Sender {
 
         let retries = this.retries;
         while (retries) {
-            const doujin = await api.fetchDoujin(Math.floor(Math.random() * 400000));
+            const doujin = await api.fetchDoujin(await api.randomDoujinID());
             if (doujin && !this.isProhibited(doujin) && this.isEnglish(doujin)) {
                 return doujin;
             }

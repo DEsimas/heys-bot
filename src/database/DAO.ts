547df51a79d7981de92a732a9a1509e8ff708f41
Serafim Sukharev <simasuh@gmail.com>
2022-06-22 13:56:54 +0300

    DB connection method changed

--- src/database/DAO.ts
+++ src/database/DAO.ts
@@ -1,19 +1,14 @@
-import { connect } from "mongoose";
 import { Prefixes } from "./Prefixes";
-import { ServersBlacklists } from "./ServersBlacklists";
 import { UsersBlacklists } from "./UsersBlacklists";
+import { ServersBlacklists } from "./ServersBlacklists";
+import { Connection, createConnection } from "mongoose";
 
 export class DAO {
-    public static readonly Prefixes = new Prefixes();
-    public static readonly ServersBalacklists = new ServersBlacklists();
-    public static readonly UsersBlacklists = new UsersBlacklists();
+    public static Prefixes: Prefixes;
+    public static ServersBalacklists: ServersBlacklists;
+    public static UsersBlacklists: UsersBlacklists;
 
     public static async connect(uri: string): Promise<void> {
-        connect(uri, error => {
-            if (error) {
-                throw new Error("Failed to connect to Mongo");
-            }
-            console.log("Database connected successfully"); 
-        });
+        const connection = createConnection(uri);
     }
 };

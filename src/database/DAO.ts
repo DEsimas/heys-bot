d9dc3eaa7f0358960fc7d3fc772707d2a8e6a49f
Serafim Sukharev <simasuh@gmail.com>
2022-06-29 21:34:59 +0300

    usersratings collection added to the DAO

--- src/database/DAO.ts
+++ src/database/DAO.ts
@@ -1,5 +1,6 @@
 import { Prefixes } from "./Prefixes";
 import { PostsRatings } from "./PostsRatings";
+import { UsersRatings } from "./UsersRatings";
 import { UsersBlacklists } from "./UsersBlacklists";
 import { ServersBlacklists } from "./ServersBlacklists";
 
@@ -8,15 +9,17 @@ import { createConnection } from "mongoose";
 export class DAO {
     public static Prefixes: Prefixes;
     public static PostsRatings: PostsRatings;
-    public static ServersBalacklists: ServersBlacklists;
+    public static UsersRatings: UsersRatings;
     public static UsersBlacklists: UsersBlacklists;
+    public static ServersBalacklists: ServersBlacklists;
 
     public static async connect(uri: string): Promise<void> {
         const connection = createConnection(uri);
 
         this.Prefixes = new Prefixes(connection);
         this.PostsRatings = new PostsRatings(connection);
-        this.ServersBalacklists = new ServersBlacklists(connection);
+        this.UsersRatings = new UsersRatings(connection);
         this.UsersBlacklists = new UsersBlacklists(connection);
+        this.ServersBalacklists = new ServersBlacklists(connection);
     }
 };

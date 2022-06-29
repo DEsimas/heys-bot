d7c895acbbefd8671f7be1115d31da7bd774fb69
Serafim Sukharev <simasuh@gmail.com>
2022-06-29 21:24:16 +0300

    getusersratings adddislike method added

--- src/database/UsersRatings.ts
+++ src/database/UsersRatings.ts
@@ -52,4 +52,26 @@ export class UsersRatings {
             dislikedPosts: []
         }));
     }
+
+    public async AddDislike(userID: string, postURL: string): Promise<UserRating> {
+        const rating = await this.UsersRatingsModel.findOne({ userID: userID });
+        if(rating) {
+            if(!rating.dislikedPosts.includes(postURL)) {
+                let dislikedPosts = rating.dislikedPosts;
+                dislikedPosts.push(postURL);
+                await this.UsersRatingsModel.updateOne({ userID: userID }, { dislikedPosts: dislikedPosts });
+                return {
+                    userID: userID,
+                    likedPosts: rating.likedPosts,
+                    dislikedPosts: dislikedPosts
+                }
+            }
+            return rating;
+        }
+        else return await (new this.UsersRatingsModel({
+            userID: userID,
+            likedPosts: [],
+            dislikedPosts: []
+        }));
+    }
 }

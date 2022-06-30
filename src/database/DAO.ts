5f50a30e9bfcf5f8e3377f9ce534388237ff6659
Serafim Sukharev <simasuh@gmail.com>
2022-06-30 14:54:49 +0300

    Rating module added to the DAO

--- src/database/DAO.ts
+++ src/database/DAO.ts
@@ -1,24 +1,21 @@
+import { Rating } from "./Rating";
 import { Prefixes } from "./Prefixes";
-import { PostsRatings } from "./PostsRatings";
-import { UsersRatings } from "./UsersRatings";
 import { UsersBlacklists } from "./UsersBlacklists";
 import { ServersBlacklists } from "./ServersBlacklists";
 
 import { createConnection } from "mongoose";
 
 export class DAO {
+    public static Rating: Rating;
     public static Prefixes: Prefixes;
-    public static PostsRatings: PostsRatings;
-    public static UsersRatings: UsersRatings;
     public static UsersBlacklists: UsersBlacklists;
     public static ServersBalacklists: ServersBlacklists;
 
     public static async connect(uri: string): Promise<void> {
         const connection = createConnection(uri);
 
+        this.Rating = new Rating(connection);
         this.Prefixes = new Prefixes(connection);
-        this.PostsRatings = new PostsRatings(connection);
-        this.UsersRatings = new UsersRatings(connection);
         this.UsersBlacklists = new UsersBlacklists(connection);
         this.ServersBalacklists = new ServersBlacklists(connection);
     }
